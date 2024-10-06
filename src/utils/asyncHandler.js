62cd6f8b20995ac94fa814e7efc32d6b4fe7e897
Aniket <aniketbaranwal8090@gmail.com>
2024-10-07 00:20:05 +0530

    Added Custom api,error,async handlers

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// const asyncHandler = (requestHandler)=> {
+//     return (req,res,next)=>{
+//          Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+//      }
+//  }
+ 
+
+ 
+ // This is only used when we have to make a web request
+ 
+ const asyncHandler =(fn)=>async(req,res,next)=>{
+    try {
+       await fn(req,res,next)
+    } catch (error) {
+       res.status(error.statusCode || 500).json({
+           success:false,
+           message:error.message
+       })
+    }  
+
+}
+export {asyncHandler}
