f47062849463c61153167b0907637b91487197b9
Aniket <aniketbaranwal8090@gmail.com>
2024-10-07 00:12:44 +0530

    Mongodb connection with dotenv

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+  try {
+ const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
+ 
+ console.log('\n connected '+ connectionInstance.connection.host);
+     
+  } catch (error) {
+    console.log("mongodb Error", error);
+    process.exit(1)
+  }
+};
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./env",
+}); 
+ 
+connectDB() 
+  .then(() => {
+    const port = process.env.PORT || 3000;
+    const server = app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+
+    server.on("error", (err) => {
+      console.error(`Failed to start the server on port ${port}`, err);
+    });
+  })
+  .catch((err) => {
+    console.log("Connection failed at port", err);
+  });
