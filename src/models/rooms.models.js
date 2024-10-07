81b98ffcebb68290758d1b39a591cf496520a54c
Aniket <aniketbaranwal8090@gmail.com>
2024-10-08 00:23:36 +0530

    Updating rooms schema for awards

--- src/models/rooms.models.js
+++ src/models/rooms.models.js
@@ -1,49 +1,55 @@
-import { Schema } from 'mongoose';
+import { Schema } from "mongoose";
 
-const roomSchema = new Schema({
-  name: {
-    type: String,
-    required: true 
+const roomSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    landlord: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    tenants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    tasks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Task",
+      },
+    ],
+
+    calendarEvents: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "CalendarEvent",
+      },
+    ],
+    awards: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Award",
+      },
+    ],
+    expenses: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Expense",
+      },
+    ],
+    maintenanceRequests: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "MaintenanceRequest",
+      },
+    ],
   },
-  landlord: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-   
-  },
-  tenants: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'User' 
-    }
-  ],
-  
-  tasks: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Task' 
-    }
-  ],
+  { timestamps: true }
+);
 
-  calendarEvents: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'CalendarEvent' 
-    }
-  ],
-
-  expenses: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Expense' 
-    }
-  ],
-  maintenanceRequests: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'MaintenanceRequest'
-    }
-  ],
-
-}, { timestamps: true });
-
-export default mongoose.model('Room', roomSchema);
+export default mongoose.model("Room", roomSchema);
