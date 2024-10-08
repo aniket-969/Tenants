eda43159fdb353eb1c1ae9b6a8641fa1a165bae8
Aniket <aniketbaranwal8090@gmail.com>
2024-10-08 17:28:15 +0530

    added refresh token method

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -99,6 +99,15 @@ process.env.ACCESS_TOKEN_SECRET,
     expiresIn :process.env.ACCESS_TOKEN_EXPIRY
 })
 }
-userSchema.methods.generateRefreshToken = function(){}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id:this._id,
+       
+    },
+process.env.REFRESH_TOKEN_SECRET,
+{
+    expiresIn :process.env.REFRESH_TOKEN_EXPIRY
+})
+}
 
   export const User = mongoose.model("User",userSchema)
