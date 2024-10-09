1ccdba74ed2dd43b03e8f5a24e592d83caa8142d
Aniket <aniketbaranwal8090@gmail.com>
2024-10-09 15:22:40 +0530

    completed user controller and routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,21 +2,25 @@ import { Router } from "express";
 import { loginSchema, registerSchema } from "./../zod/user.schema.js";
 import { validate } from "../middleware/validator.middleware.js";
 import {
+  changePassword,
   loginUser,
-  logoutUser, 
-  refreshTokens, 
+  logoutUser,
+  refreshTokens,
   registerUser,
+  updateAccountDetails,
 } from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(validate(registerSchema), registerUser);
-router.route("/login").post(validate(loginSchema),loginUser);
+router.route("/login").post(validate(loginSchema), loginUser);
 
 // secured routes
-router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route("/refreshTokens").post(verifyJWT,refreshTokens);
+router.route("/refreshTokens").post(verifyJWT, refreshTokens);
+router.route("/change-password").post(verifyJWT, changePassword);
+router.route("/update-user").patch(verifyJWT, updateAccountDetails);
 
 export default router;
