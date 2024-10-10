07a6062632b0158cedfbb82eecc85758148f00d0
Aniket <aniketbaranwal8090@gmail.com>
2024-10-10 17:36:46 +0530

    Added cast vote functionality

--- src/controllers/poll.controllers.js
+++ src/controllers/poll.controllers.js
@@ -1,6 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Poll } from "../models/poll.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import mongoose from "mongoose";
 
 const createPoll = asyncHandler(async (req, res) => {
   const { title, roomId, status, voteEndTime, optionText } = req.body;
@@ -23,4 +25,30 @@ const createPoll = asyncHandler(async (req, res) => {
   return res.json(new ApiResponse(201, poll, "Poll created successfully"));
 });
 
-export { createPoll };
+const castVote = asyncHandler(async (req, res) => {
+  const { pollId, optionId } = req.body;
+  const userId = req.user?._id;
+  const poll = Poll.findById(pollId);
+  if (!poll) return new ApiError(404, "Poll not found");
+  if (new Date() > new Date(poll.voteEndTime)) {
+    throw new ApiError(400, "Voting has ended for this poll");
+  }
+
+  const userHasVoted = poll.options.some((option) =>
+    option.votes.some((vote) => vote.voter.toString() === userId.toString())
+  );
+
+  if (userHasVoted) throw new ApiError(400, "User already have voted");
+
+  const option = poll.options.id(optionId);
+  if (!option) {
+    throw new ApiError(404, "Option not found");
+  }
+
+  option.votes.push({ voter: mongoose.Types.ObjectId(userId) });
+
+  await poll.save();
+  return res.json(new ApiResponse(200, poll, "Vote cast successfully"));
+});
+
+export { createPoll, castVote };
--- src/routes/vote.routes.js
+++ src/routes/vote.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { validate } from "../middleware/validator.middleware.js";
 import { pollSchema } from "../zod/poll.schema.js";
 import { checkMember } from "../middleware/poll.middleware.js";
-import { createPoll } from "../controllers/poll.controllers.js";
+import { castVote, createPoll } from "../controllers/poll.controllers.js";
 
 const router = Router()
 
-router.route("/create-vote").post(validate(pollSchema),checkMember,createPoll)
+router.route("/create-poll").post(validate(pollSchema),checkMember,createPoll)
+
+router.route("/cast-vote").post(castVote)
 
 export default router
