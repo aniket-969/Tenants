435f652c00f7b183cf68bcc9c83363daf5d0d941
Aniket <aniketbaranwal8090@gmail.com>
2024-10-13 14:57:37 +0530

    Mofigyint the schema

--- src/models/rooms.model.js
+++ src/models/rooms.model.js
@@ -1,5 +1,42 @@
 import mongoose, { Schema } from "mongoose";
 
+const maintenanceSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in_progress", "resolved", "cancelled"],
+      default: "pending",
+    },
+    maintenanceProvider: {
+      type: String,
+      default: null,
+    },
+    contactPhone: {
+      type: String,
+      default: null,
+    },
+    costEstimate: {
+      type: Number,
+      default: 0,
+    },
+    dateReported: {
+      type: Date,
+      default: Date.now,
+    },
+    dateResolved: {
+      type: Date,
+    },
+  },
+)
+
 const roomSchema = new Schema(
   {
     name: {
@@ -35,40 +72,7 @@ const roomSchema = new Schema(
       },
     ],
     maintenanceRequests: [
-      {
-        title: {
-          type: String,
-          required: true,
-        },
-        description: {
-          type: String,
-          required: true,
-        },
-        status: {
-          type: String,
-          enum: ["pending", "in_progress", "resolved", "cancelled"],
-          default: "pending",
-        },
-        maintenanceProvider: {
-          type: String,
-          default: null,
-        },
-        contactPhone: {
-          type: String,
-          default: null,
-        },
-        costEstimate: {
-          type: Number,
-          default: 0,
-        },
-        dateReported: {
-          type: Date,
-          default: Date.now,
-        },
-        dateResolved: {
-          type: Date,
-        },
-      },
+      [maintenanceSchema]
     ],
   },
   { timestamps: true }
