3c4f55d11548c64d30cb24aac45f5de67aad542e
Aniket <aniketbaranwal8090@gmail.com>
2024-10-16 22:21:33 +0530

    Refactored few bugs

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -1,15 +1,15 @@
 
 import { Router } from 'express';
 import { verifyJWT } from './../middleware/auth.middleware.js';
-import { createExpense, deleteExpense, getExpenseDetails, getPendingPayments, getUserExpenses, updatePayment } from '../controllers/expense.controller.js';
+import { createExpense, deleteExpense, getExpenseDetails, getPendingPayments, getUserExpenses, updateExpense, updatePayment } from '../controllers/expense.controller.js';
 
 const router = Router()
 router.route("/create-expense").post(verifyJWT,createExpense)
 router.route("/update-expense").patch(verifyJWT,updatePayment)
 router.route("/user-expense").get(verifyJWT,getUserExpenses)
-router.route("/pendingExpense").get(verifyJWT,getPendingPayments)
+router.route("/pending").get(verifyJWT,getPendingPayments)
 router.route("/:expenseId").get(verifyJWT,getExpenseDetails)
-router.route("/:expenseId").patch(verifyJWT,updatePayment)
+router.route("/:expenseId").patch(verifyJWT,updateExpense)
 router.route("/:expenseId").delete(verifyJWT,deleteExpense)
 
 export default router
