d31255acd2d11b1cb8631eb4ea57b2e947318de4
Aniket <aniketbaranwal8090@gmail.com>
2024-10-17 22:55:29 +0530

    Modifying the routes for awards

--- src/controllers/awards.controller.js
+++ src/controllers/awards.controller.js
@@ -3,37 +3,17 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const getRoomAwards = asyncHandler(async (req, res) => {
-  const { roomId } = req.params;
-  const room = await Room.findById(roomId).populate("tenants landlord");
-  if (!room) throw new ApiError(404, "Room not found");
-
-  const roomUsers = [...room.tenants, room.landlord];
-
-  const awards = await AwardTemplate.find({
-    awardedTo: { $in: roomUsers },
-  }).populate("awardTemplate awardedTo");
-
-  if (!awards.length) {
-    throw new ApiError(404, "No awards found");
-  }
-
-  return res.json(
-    new ApiResponse(200, awards, "Room awards fetched successfully")
-  );
-});
-
 const customRoomAward = asyncHandler(async (req, res) => {
-  const { roomId, title, description, image, criteria,assignedTo } = req.body;
+  const { roomId, title, description, image, criteria, assignedTo } = req.body;
 
-  const room =await Room.findById(roomId);
+  const room = await Room.findById(roomId);
 
   const award = {
     title,
     description,
     image,
     criteria,
-    assignedTo
+    assignedTo,
   };
 
   room.awards.push(award);
@@ -46,4 +26,4 @@ const customRoomAward = asyncHandler(async (req, res) => {
   );
 });
 
-export { getRoomAwards,customRoomAward };
+export { customRoomAward };
--- src/routes/awards.routes.js
+++ src/routes/awards.routes.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import {
   customRoomAward,
-  getRoomAwards,
 } from "../controllers/awards.controller.js";
 import { verifyJWT } from "./../middleware/auth.middleware.js";
 import { checkMember } from "../middleware/poll.middleware.js";
 
 const router = Router();
 
-router.route("/roomAwards", getRoomAwards);
-router.route("/roomAwards").post(verifyJWT, checkMember, customRoomAward);
+router.route("/create").post(verifyJWT, checkMember, customRoomAward);
 
 export default router;
