2e8536b5a6909b02f979fcdb2dcdd46faa96f0e9
Aniket <aniketbaranwal8090@gmail.com>
2024-10-17 12:24:18 +0530

    Improving roomSchema

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,7 +1,36 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { Room } from "../models/rooms.model.js";
 
-const createRoomTasks = asyncHandler(async(req,res)=>{
-    
-})
+const createRoomTasks = asyncHandler(async (req, res) => {
+  const createdBy = req.user?._id;
+  const {
+    title,
+    description,
+    currentAssignee,
+    dueDate,
+    participants,
+    rotationOrder,
+    completed,
+    priority,
+    recurring,
+    recurrencePattern,
+    customRecurrence,
+  } = req.body;
+  const room = await Room.findById(roomId);
+  const task = {
+    title,
+    description,
+    currentAssignee,
+    dueDate,
+    participants,
+    rotationOrder,
+    completed,
+    priority,
+    recurring,
+    recurrencePattern,
+    customRecurrence,
+  };
+  room.tasks.push(task);
+});
--- src/models/rooms.model.js
+++ src/models/rooms.model.js
@@ -1,140 +1,5 @@
 import mongoose, { Schema } from "mongoose";
 
-const maintenanceSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "in_progress", "resolved", "cancelled"],
-    default: "pending",
-  },
-  maintenanceProvider: {
-    type: String,
-    default: null,
-  },
-  contactPhone: {
-    type: String,
-    default: null,
-  },
-  costEstimate: {
-    type: Number,
-    default: 0,
-  },
-  dateReported: {
-    type: Date,
-    default: Date.now,
-  },
-  dateResolved: {
-    type: Date,
-  },
-});
-
-const awardSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-    },
-    image: {
-      type: String,
-    },
-    criteria: {
-      type: String,
-    },
-  },
-  { timestamps: true }
-);
-
-const taskSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-    },
-    currentAssignee: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    createdBy: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    dueDate: {
-      type: Date,
-      required: true,
-    },
-    participants: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "user",
-        required: true,
-      },
-    ],
-    rotationOrder: {
-      type: [Schema.Types.ObjectId],
-      ref: "User",
-    },
-    completed: {
-      type: Boolean,
-      default: false,
-    },
-    priority: {
-      type: String,
-      enum: ["low", "medium", "high"],
-      default: "medium",
-    },
-    switches: [
-      {
-        requestedBy: {
-          type: Schema.Types.ObjectId,
-          ref: "User",
-        },
-        acceptedBy: {
-          type: Schema.Types.ObjectId,
-          ref: "User",
-        },
-      },
-    ],
-    switchCountPerUser: [
-      {
-        user: { type: Schema.Types.ObjectId, ref: "User" },
-        requestCount: { type: Number, default: 0 },  
-        acceptCount: { type: Number, default: 0 },   
-      },
-    ],
-    completedBy: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-    recurring: {
-      type: Boolean,
-      default: false,
-    },
-    recurrencePattern: {
-      type: String,
-      enum: ["daily", "weekly", "monthly", "custom"],
-    },
-    customRecurrence: {
-      type: String,
-    },
-  },
-  { timestamps: true }
-);
-
 const roomSchema = new Schema(
   {
     name: {
@@ -151,19 +16,151 @@ const roomSchema = new Schema(
         ref: "User",
       },
     ],
-    awards: [awardSchema],
-    maintenanceRequests: [maintenanceSchema],
-    tasks: [taskSchema],
+    awards: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        description: {
+          type: String,
+        },
+        image: {
+          type: String,
+        },
+        criteria: {
+          type: String,
+        },
+      },
+    ],
+    maintenanceRequests: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        description: {
+          type: String,
+          required: true,
+        },
+        status: {
+          type: String,
+          enum: ["pending", "in_progress", "resolved", "cancelled"],
+          default: "pending",
+        },
+        maintenanceProvider: {
+          type: String,
+          default: null,
+        },
+        contactPhone: {
+          type: String,
+          default: null,
+        },
+        costEstimate: {
+          type: Number,
+          default: 0,
+        },
+        dateReported: {
+          type: Date,
+          default: Date.now,
+        },
+        dateResolved: {
+          type: Date,
+        },
+      },
+    ],
+    tasks: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        description: {
+          type: String,
+        },
+        currentAssignee: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        createdBy: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        dueDate: {
+          type: Date,
+          required: true,
+        },
+        participants: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+          },
+        ],
+        rotationOrder: {
+          type: [Schema.Types.ObjectId],
+          ref: "User",
+        },
+        completed: {
+          type: Boolean,
+          default: false,
+        },
+        priority: {
+          type: String,
+          enum: ["low", "medium", "high"],
+          default: "medium",
+        },
+        switches: [
+          {
+            requestedBy: {
+              type: Schema.Types.ObjectId,
+              ref: "User",
+            },
+            acceptedBy: {
+              type: Schema.Types.ObjectId,
+              ref: "User",
+            },
+          },
+        ],
+        switchCountPerUser: [
+          {
+            user: { type: Schema.Types.ObjectId, ref: "User" },
+            requestCount: { type: Number, default: 0 },
+            acceptCount: { type: Number, default: 0 },
+          },
+        ],
+        completedBy: {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+        recurring: {
+          type: Boolean,
+          default: false,
+        },
+        recurrencePattern: {
+          type: String,
+          enum: ["daily", "weekly", "monthly", "custom"],
+        },
+        customRecurrence: {
+          type: String,
+        },
+      },
+    ],
     votes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Vote" }],
   },
   { timestamps: true }
 );
 
-taskSchema.index({ dueDate: 1 });
-taskSchema.index({ currentAssignee: 1 });
-taskSchema.index({ dueDate: 1 });
-taskSchema.index({ recurring: 1, recurrencePattern: 1 });
-taskSchema.index({ createdBy: 1 });
-taskSchema.index({ participants: 1 });
+roomSchema.index({ "tasks.dueDate": 1 });
+roomSchema.index({ "tasks.currentAssignee": 1 });
+roomSchema.index({ "tasks.createdBy": 1 });
+roomSchema.index({ "tasks.participants": 1 });
+roomSchema.index({ "tasks.recurring": 1, "tasks.recurrencePattern": 1 });
+roomSchema.index({ "maintenanceRequests.status": 1 });
+roomSchema.index({ "maintenanceRequests.dateReported": 1 });
+roomSchema.index({ "maintenanceRequests.dateResolved": 1 });
+
 
 export const Room = mongoose.model("Room", roomSchema);
