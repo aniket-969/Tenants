faabd291986e777820510dd0066e398777deea7c
Aniket <aniketbaranwal8090@gmail.com>
2024-10-18 11:43:23 +0530

    Modifying createroom according to new schmea

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -2,41 +2,39 @@ import { Room } from "../models/rooms.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import crypto from "crypto"
-
+import crypto from "crypto";
 
 function generateGroupCode() {
-    return crypto.randomBytes(6).toString('hex').slice(0, 6).toUpperCase();
-  }
-  
-  async function generateUniqueGroupCode() {
-    let isUnique = false;
-    let groupCode;
-  
-    while (!isUnique) {
-      groupCode = generateGroupCode();
-  
-      const existingRoom = await Room.findOne({ groupCode });
-      
-      if (!existingRoom) {
-        isUnique = true;
-      }
-    }
-  
-    return groupCode;
-  }
-  
+  return crypto.randomBytes(6).toString("hex").slice(0, 6).toUpperCase();
+}
 
+async function generateUniqueGroupCode() {
+  let isUnique = false;
+  let groupCode;
+
+  while (!isUnique) {
+    groupCode = generateGroupCode();
+
+    const existingRoom = await Room.findOne({ groupCode });
+
+    if (!existingRoom) {
+      isUnique = true;
+    }
+  }
+
+  return groupCode;
+}
 
 const createRoom = asyncHandler(async (req, res) => {
   const admin = req.user?._id;
-  const { name, password, description, role } = req.body;
+  const { name, description, role } = req.body;
 
+  const groupCode = await generateUniqueGroupCode();
   let roomData = {
     name,
-    password,
     description,
     admin,
+    groupCode
   };
 
   if (role === "landlord") {
@@ -47,7 +45,7 @@ const createRoom = asyncHandler(async (req, res) => {
 
   const room = await Room.create(roomData);
 
-  const createdRoom = await Room.findById(room._id).select("-password");
+  const createdRoom = await Room.findById(room._id);
   if (!createdRoom) {
     throw new ApiError(400, "Error creating room");
   }
@@ -58,9 +56,7 @@ const createRoom = asyncHandler(async (req, res) => {
 });
 
 const addUser = asyncHandler(async (req, res) => {
-  const { roomId,password } = req.body;
-
-
+  const { roomId, password } = req.body;
 });
 
 export { createRoom };
