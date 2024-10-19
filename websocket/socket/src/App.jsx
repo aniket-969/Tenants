a031f2361e882223d94c3635b5f14277a53e0b32
Aniket <aniketbaranwal8090@gmail.com>
2024-10-20 01:34:41 +0530

    real time communicaiton with socket

--- websocket/socket/src/App.jsx
+++ websocket/socket/src/App.jsx
@@ -1,26 +1,42 @@
-import { useState,useEffect } from 'react'
-import './App.css'
-import {io} from "socket.io-client"
+import { useState, useEffect } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
 
 function App() {
-useEffect(()=>{
- socket.on("connect",()=>{
-  console.log("connected")
-  console.log(socket.id)
+  const socket = io("http://localhost:3000");
+const [me, setMe] = useState(" ");
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected");
+      console.log(socket.id);
+      console.log(me)
+      return () => {
+        socket.disconnect();
+      };
+    });
+socket.on("message",(data)=>{
+  console.log(data)
+})
+    socket.on("Welcome", (s) => {
+      console.log(s);
+    });
+  }, []);
   
- }) 
+  const handleForm = () => {
+  
+    console.log(me);
+    socket.emit("message", me);
+    setMe("");
+  };
 
- socket.on("Welcome",(s)=>{
-  console.log(s)
- })
-},[])
-  const socket = io("http://localhost:3000")
-    
   return (
-   <div>
-    web socket
-   </div>
-  )
+    <div>
+        <input value={me} type="text" onChange={(e) => setMe(e.target.value)} />
+        <button onClick = {handleForm}>submit</button>
+     
+      {me}
+    </div>
+  );
 }
 
-export default App 
+export default App;
