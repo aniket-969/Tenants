e2f552be928901aae42cb3cc12413396f37a73e8
Aniket <aniketbaranwal8090@gmail.com>
2024-10-22 17:28:34 +0530

    Adding update zod schema for maintenance

--- src/routes/maintenance.routes.js
+++ src/routes/maintenance.routes.js
@@ -3,11 +3,11 @@ import { createMaintenance, deleteMaintenance, updateMaintenance } from "../cont
 import { verifyJWT } from "./../middleware/auth.middleware.js";
 import { checkMember } from "../middleware/poll.middleware.js";
 import { validate } from './../middleware/validator.middleware.js';
-import { createMaintenanceSchema } from "../zod/maintenance.schema.js";
+import { createMaintenanceSchema, updateMaintenaceSchema } from "../zod/maintenance.schema.js";
 const router = Router();
 
-router.route("/maintenance").post(verifyJWT,validate(createMaintenanceSchema), checkMember, createMaintenance);
-router.route("/maintenance").delete(verifyJWT, checkMember, deleteMaintenance);
-router.route("/maintenance").patch(verifyJWT, checkMember, updateMaintenance);
+router.route("/create").post(verifyJWT,validate(createMaintenanceSchema), checkMember, createMaintenance);
+router.route("/delete").delete(verifyJWT, checkMember, deleteMaintenance);
+router.route("/update").patch(verifyJWT,validate(updateMaintenaceSchema), checkMember, updateMaintenance);
 
 export default router;
--- src/zod/maintenance.schema.js
+++ src/zod/maintenance.schema.js
@@ -12,3 +12,16 @@ export const createMaintenanceSchema = z.object({
     costEstimate:z.number().min(1,{message:"Cost estimate should contain atleast 1 digit"}).max(10,{message:"Cost estimate can't exceed above 10 digits"}).optional(),
 })
 
+export const updateMaintenaceSchema = z.object({
+    status:z.enum(["pending", "in_progress", "resolved", "cancelled"]).optional(),
+    roomId:objectIdValidation,
+    title:stringValidation(1,20,"title").optional(),
+    description:stringValidation(1,100,"description").optional(),
+    maintenanceProvider:stringValidation(1,20,"maintenaceProvider").optional(),
+    contactPhone:stringValidation(10,15,"contactPhone").regex(/^\+?[0-9]+$/, {
+        message: 'Phone number must contain only digits and an optional leading + for international numbers.',
+      }).optional(),
+    costEstimate:z.number().min(1,{message:"Cost estimate should contain atleast 1 digit"}).max(10,{message:"Cost estimate can't exceed above 10 digits"}).optional(),
+    maintenanceId:objectIdValidation,
+    dateResolved:z.string().date().optional(),
+})
