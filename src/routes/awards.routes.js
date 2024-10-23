6e66ccd5e834962df48c8ca527c5c71520942bfa
Aniket <aniketbaranwal8090@gmail.com>
2024-10-23 18:51:25 +0530

    Created update award zod validation

--- src/routes/awards.routes.js
+++ src/routes/awards.routes.js
@@ -7,12 +7,12 @@ import {
 import { verifyJWT } from "./../middleware/auth.middleware.js";
 import { checkMember } from "../middleware/poll.middleware.js";
 import { validate } from './../middleware/validator.middleware.js';
-import { createCustomAwardSchema } from "../zod/awards.schema.js";
+import { createCustomAwardSchema, updateAwardSchema } from "../zod/awards.schema.js";
 
 const router = Router();
 
 router.route("/create").post(verifyJWT, validate(createCustomAwardSchema),checkMember, customRoomAward);
 router.route("/delete").delete(verifyJWT, checkMember, deleteRoomAward);
-router.route("/update").patch(verifyJWT, checkMember, updateRoomAward);
+router.route("/update").patch(verifyJWT,validate(updateAwardSchema), checkMember, updateRoomAward);
  
 export default router;
--- src/zod/awards.schema.js
+++ src/zod/awards.schema.js
@@ -9,3 +9,13 @@ export const createCustomAwardSchema = z.object({
   criteria: stringValidation(1, 20, "criteria").optional(),
   assignedTo: objectIdValidation,
 });
+
+export const updateAwardSchema = z.object({
+    roomId: objectIdValidation,
+    awardId:objectIdValidation,
+    title: stringValidation(1, 20, "title").optional(),
+    description: stringValidation(1, 50, "description").optional(),
+    image: stringValidation(1, 300, "image").optional(),
+    criteria: stringValidation(1, 20, "criteria").optional(),
+    assignedTo: objectIdValidation.optional(),
+})
