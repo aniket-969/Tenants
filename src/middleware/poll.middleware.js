21fb18a88d4c614340fa7fe03b5219a39ff4ff2a
Aniket <aniketbaranwal8090@gmail.com>
2024-10-23 18:58:14 +0530

    Modifying checkMember middleware for params

--- src/middleware/poll.middleware.js
+++ src/middleware/poll.middleware.js
@@ -3,7 +3,10 @@ import { Room } from "../models/rooms.model.js";
 import { ApiError } from "../utils/ApiError.js";
 
 const checkMember = asyncHandler(async (req, res) => {
-  const { roomId } = req.body;
+  const roomId = req.body?.roomId || req.params?.roomId;
+if(!roomId){
+  throw new ApiError(404,"Room Id is required")
+}
   const createdBy = req.user?._id;
   const room = await Room.findById(roomId);
   if (!room) throw new ApiError(404, "Room not found");
