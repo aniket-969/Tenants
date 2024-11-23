7f6d80d02b7b2c28c56cec4895faee5e0861479f
Aniket <aniketbaranwal8090@gmail.com>
2024-11-23 18:42:08 +0530

    Added group chatmessage model

--- src/models/chat.model.js
+++ src/models/chat.model.js
@@ -1,6 +1,4 @@
 import mongoose, { Schema } from "mongoose";
-import {Room} from '../models/rooms.model.js';
-import {User} from "../models/user.model.js"
 
 const chatSchema = new Schema(
   {
---
+++ src/models/chatMessage.model.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+const chatMessageSchema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    attachments: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+          type: {
+            type: String,
+            enum: ["image", "pdf", "other"],
+            default: "other",
+          },
+        },
+      ],
+      default: [],
+    },
+    chat: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema);
