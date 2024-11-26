a4feafaa3ae192453c6c5df22e7569fb27ebc10c
Aniket <aniketbaranwal8090@gmail.com>
2024-11-26 21:14:32 +0530

    Adding shadcn/ui setup with tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './styles/globals.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     h
+    <div className="text-3xl font-bold underline">sdfa</div>
     </>
   )
 }
