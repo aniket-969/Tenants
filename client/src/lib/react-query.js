8ca53a8b903bf2d21b41e343a4117142ce87aa28
Aniket <aniketbaranwal8090@gmail.com>
2024-11-29 18:26:22 +0530

    Adding tanstack query client provider

---
+++ client/src/lib/react-query.js
@@ -0,0 +1,10 @@
+
+
+export const queryConfig = {
+  queries: {
+    // throwOnError: true,
+    refetchOnWindowFocus: false,
+    retry: false,
+    staleTime: 1000 * 60,
+  },
+} 
--- client/src/pages/app/provider.jsx
+++ client/src/pages/app/provider.jsx
@@ -1,24 +1,37 @@
 import { Spinner } from "@/components/ui/spinner"
+import { queryConfig } from "@/lib/react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 import { ErrorBoundary } from "react-error-boundary"
 
-export const AppProvider = ({children})=>{
+
+export const AppProvider = ({ children }) => {
+
+    const [queryClient] = React.useState(
+        () =>
+            new QueryClient({
+                defaultOptions: queryConfig,
+            }),
+    );
+
     return (
-        <React.Suspense  fallback={
+        <React.Suspense fallback={
             <div className="flex h-screen w-screen items-center justify-center">
-              <Spinner size="xl" />
+                <Spinner size="xl" />
             </div>
-          }>
+        }>
             <ErrorBoundary FallbackComponent={({ error, resetErrorBoundary }) => (
-    <div>
-        <p>Something went wrong: {error.message}</p>
-        <button onClick={resetErrorBoundary}>Retry</button>
-    </div>
-)}>
-    {children}
-</ErrorBoundary>
+                <div>
+                    <p>Something went wrong: {error.message}</p>
+                    <button onClick={resetErrorBoundary}>Retry</button>
+                </div>
+            )}>
+                <QueryClientProvider client={queryClient}>
+
+
+                    {children}
+                </QueryClientProvider>
+            </ErrorBoundary>
         </React.Suspense>
     )
 }
-
-
