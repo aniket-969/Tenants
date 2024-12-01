5461d8e24eed93d8d80c7ab873cb85a219e3f90b
Aniket <aniketbaranwal8090@gmail.com>
2024-12-01 21:39:38 +0530

    Declaring all the api routes in auth.js for user

--- client/src/api/queries/auth.js
+++ client/src/api/queries/auth.js
@@ -1,7 +1,27 @@
 import axiosClient from "../axiosClient";
 
-const baseAuth = 'users'
+const baseAuth = "users";
 
-export const registerUser = async(data)=>{
-    return axiosClient.post(`/${baseAuth}/register`,data)
-}
+export const registerUser = async (data) => {
+  return axiosClient.post(`/${baseAuth}/register`, data);
+};
+
+export const loginUser = async (data) => {
+  return axiosClient.post(`/${baseAuth}/login`, data);
+};
+
+export const logOut = async (data) => {
+  return axiosClient.post(`/${baseAuth}/logout`, data);
+};
+
+export const refreshTokens = async (data) => {
+  return axiosClient.post(`/${baseAuth}/refreshTokens`, data);
+};
+
+export const updateUser = async (data) => {
+  return axiosClient.patch(`/${baseAuth}/update-user`, data);
+};
+
+export const changePassword = async (data) => {
+  return axiosClient.patch(`/${baseAuth}/change-password`, data);
+};
