eee1dbc645a0462f31766fe47d8a6a734d125d62
Aniket <aniketbaranwal8090@gmail.com>
2024-12-02 18:18:57 +0530

    Modifying event routes

--- server/src/routes/event.routes.js
+++ server/src/routes/event.routes.js
@@ -14,11 +14,13 @@ import { checkMember } from "../middleware/poll.middleware.js";
 const router = Router();
 
 router
-  .route("/create") 
+  .route("/") 
   .post(verifyJWT, validate(createCalendarEventSchema), createCalendarEvent);
-router.route("/:eventId").delete(verifyJWT,checkMember, deleteCalendarEvent);
-router.route("/:roomId").get(verifyJWT,checkMember, getRoomCalendarEvent);
-router.route("/:eventId").get(verifyJWT,checkMember,getSingleEvent)
-router.route("/room/:roomId/events/monthly").get(verifyJWT,checkMember,getMonthlyEvents)
+  router
+  .route("/:eventId")
+  .delete(verifyJWT, checkMember, deleteCalendarEvent)
+  .get(verifyJWT, checkMember, getSingleEvent);
+router.route("/room/:roomId").get(verifyJWT,checkMember, getRoomCalendarEvent);
+router.route("/room/:roomId/monthly").get(verifyJWT,checkMember,getMonthlyEvents)
  
 export default router;
