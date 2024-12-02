1a8e0707d204ddb97c2f57ccf32ff061650194e4
Aniket <aniketbaranwal8090@gmail.com>
2024-12-02 15:28:09 +0530

    Modifying routes for rest API arch

--- server/src/routes/room.routes.js
+++ server/src/routes/room.routes.js
@@ -4,16 +4,16 @@ import { verifyJWT } from './../middleware/auth.middleware.js';
 import { checkMember } from "../middleware/poll.middleware.js";
 import { addUserRequestSchema, adminResponseSchema, creatRoomSchema, transferRoleSchema } from "../zod/room.schema.js";
 import { validate } from './../middleware/validator.middleware.js';
-
+ 
 const router = Router();
 
-router.route("/create").post(verifyJWT,validate(creatRoomSchema),createRoom);
-router.route("/request").post(verifyJWT,validate(addUserRequestSchema),addUserRequest);
-router.route("/response").post(verifyJWT,validate(adminResponseSchema),adminResponse);
-router.route("/update/:roomId").patch(verifyJWT,updateRoom);
-router.route("/delete/:roomId").delete(verifyJWT,deleteRoom);
+router.route("/").post(verifyJWT,validate(creatRoomSchema),createRoom);
+router.route("/:roomId/requests").post(verifyJWT,validate(addUserRequestSchema),addUserRequest);
+router.route("/:roomId/responses").post(verifyJWT,validate(adminResponseSchema),adminResponse);
+router.route("/:roomId").patch(verifyJWT,updateRoom);
+router.route("/:roomId").delete(verifyJWT,deleteRoom);
 router.route("/:roomId").get(verifyJWT,checkMember,getRoomData);
-router.route("/leave/:roomId").patch(verifyJWT,checkMember,getRoomData);
-router.route("/admin/tranfer").post(verifyJWT,validate(transferRoleSchema),checkMember,transferAdminControl);
+router.route("/:roomId/leave").patch(verifyJWT,checkMember,getRoomData);
+router.route("/:roomId/admin/transfer").post(verifyJWT,validate(transferRoleSchema),checkMember,transferAdminControl);
 
 export default router;
