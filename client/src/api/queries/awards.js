a654ccfad309c0e87033ea1295c27d4f71dbd76b
Aniket <aniketbaranwal8090@gmail.com>
2024-12-02 20:42:17 +0530

    Adding axios route for api for awards

---
+++ client/src/api/queries/awards.js
@@ -0,0 +1,15 @@
+import axiosClient from "../axiosClient"
+
+const baseAward = "awards"
+
+export const createRoomAward = async((roomId,data)=>{
+    return axiosClient.post(`/${baseAward}/${roomId}`,data)
+})
+
+export const deleteRoomAward = async((roomId,awardId)=>{
+    return axiosClient.delete(`/${baseAward}/${roomId}/${awardId}`)
+})
+
+export const updateRoomAward = async((roomId,awardId,data)=>{
+    return axiosClient.patch(`/${baseAward}/${roomId}/${awardId}`,data)
+})
