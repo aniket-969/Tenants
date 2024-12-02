d8bf95ccd7560d8c7107e5e0ae812f01d3a8b158
Aniket <aniketbaranwal8090@gmail.com>
2024-12-02 18:54:44 +0530

    Adding maintenace routes for axios api calls

---
+++ client/src/api/queries/maintenance.js
@@ -0,0 +1,16 @@
+import axiosClient from "../axiosClient";
+
+const baseMaintenance = "maintenance"
+
+export const createMaintenance = async((roomId)=>{
+    return axiosClient.post(`/${roomId}`)
+})
+
+export const deleteMaintenance = async((roomId,maintenanceId)=>{
+    return axiosClient.delete(`/${maintenanceId}/${roomId}`)
+})
+
+export const updateMaintenance = async((roomId,maintenanceId)=>{
+    return axiosClient.patch(`/${maintenanceId}/${roomId}`)
+})
+
