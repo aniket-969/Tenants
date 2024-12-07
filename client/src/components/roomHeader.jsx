c5e5287b63819aba6d24f03b4b33826b9db6bee0
Aniket <aniketbaranwal8090@gmail.com>
2024-12-07 19:02:55 +0530

    initial navbar setup

--- client/src/components/roomHeader.jsx
+++ client/src/components/roomHeader.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
 
 export const RoomHeader = () => {
   return (
-    <div>roomHeader</div>
-  )
-}
+    <div className="flex items-center justify-between p-3 bg-gray-100 shadow-md">
+      {/* Leftmost Title: Dashboard */}
+     <p className="text-background font-semibold text-lg">  Dashboard</p>
+      
+      {/* Navigation Links */}
+      <div className="flex space-x-4">
+        <Link
+          to="/room/create"
+          className=" "
+        >
+            <Button  variant="link" size="sm"> Create/Join Room</Button>
+         
+        </Link>
+       
+        <Link
+          to="/"
+          
+        >
+            <Button variant="link" size="sm">Logout</Button>
+          
+        </Link>
+      </div>
+    </div>
+  );
+};
