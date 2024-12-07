d4bff062cade2d2c142ed636cfa0fc7cbe797c4d
Aniket <aniketbaranwal8090@gmail.com>
2024-12-07 11:36:51 +0530

    Adding sparkle effect to award card

--- client/src/components/awardCard.jsx
+++ client/src/components/awardCard.jsx
@@ -2,8 +2,23 @@ import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "./ui/
 
 const AwardCard = ({title,description,image,criteria,assignedTo})=>{
     return(
-        <Card className="w-full">
-      <img src={image} alt={title} className="h-40 w-full object-cover rounded-t-md" />
+        <Card className="w-full relative overflow-hidden bg-white group">
+      {/* Image */}
+      <div className="relative">
+        <img src={image} alt={title} className="h-40 w-full object-cover rounded-t-md" />
+      </div>
+
+      {/* Star Sparkle Effect */}
+      <div className="absolute inset-0 pointer-events-none">
+        <div className="sparkle-container">
+          {/* Sparkles */}
+          {[...Array(10)].map((_, index) => (
+            <div key={index} className="sparkle" />
+          ))}
+        </div>
+      </div>
+
+      {/* Card Content */}
       <CardHeader>
         <CardTitle>{title}</CardTitle>
         {criteria && <CardDescription>Criteria: {criteria}</CardDescription>}
