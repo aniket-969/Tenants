2b8381293d3a4a19c9683a79112c28f35521368d
Aniket <aniketbaranwal8090@gmail.com>
2024-12-07 22:08:12 +0530

    QR code scanner working

--- client/src/components/roomHeader.jsx
+++ client/src/components/roomHeader.jsx
@@ -3,26 +3,23 @@ import { Button } from "./ui/button";
 
 export const RoomHeader = () => {
   return (
-    <div className="flex items-center justify-between p-3 bg-gray-100 shadow-md">
+    <div className="flex items-center justify-between p-3 shadow-md bg-white">
       {/* Leftmost Title: Dashboard */}
-     <p className="text-background font-semibold text-lg">  Dashboard</p>
-      
+      <p className="text- font-semibold text-lg text-primary"> Dashboard</p>
+
       {/* Navigation Links */}
       <div className="flex space-x-4">
-        <Link
-          to="/room/create"
-          className=" "
-        >
-            <Button  variant="link" size="sm"> Create/Join Room</Button>
-         
+        <Link to="/room/create" className=" ">
+          <Button variant="link" size="sm" className="text-primary ">
+            {" "}
+            Create/Join Room
+          </Button>
         </Link>
-       
-        <Link
-          to="/"
-          
-        >
-            <Button variant="link" size="sm">Logout</Button>
-          
+
+        <Link to="/">
+          <Button className="text-primary " variant="link" size="sm">
+            Logout
+          </Button>
         </Link>
       </div>
     </div>
--- client/src/pages/room/Room.jsx
+++ client/src/pages/room/Room.jsx
@@ -1,6 +1,7 @@
 import ProfileCard from '@/components/profileCard'
 import QRCode from '@/components/QRCode'
 import { RoomHeader } from '@/components/roomHeader'
+import RoomList from '@/components/roomList'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
@@ -9,6 +10,7 @@ const Room = () => {
     <div>
       <RoomHeader/>
       <ProfileCard/>
+      <RoomList/>
       <QRCode/>
     </div>
   )
