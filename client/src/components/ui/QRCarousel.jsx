d7ff43d60b16c85446cd2ed2e6a6b5f0cfa244ec
Aniket <aniketbaranwal8090@gmail.com>
2024-12-09 11:26:29 +0530

    Qr carousel working

--- client/src/components/ui/QRCarousel.jsx
+++ client/src/components/ui/QRCarousel.jsx
@@ -1,5 +1,14 @@
 import { generateQRCode } from "@/utils/helper";
 import { useEffect, useState } from "react";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+import { Card, CardContent  } from "./card";
+  
 
 export const QRCarousel = ({ paymentMethod }) => {
   const [qrImages, setQrImages] = useState({});
@@ -20,22 +29,33 @@ export const QRCarousel = ({ paymentMethod }) => {
 
   console.log(paymentMethod);
   return (
-    <div className="">
-      {paymentMethod.map((payment) => (
-        <div key={payment._id}>
-          <p>{payment.appName}</p>
-          <p>{payment.paymentId}</p>
-          <p>{payment.type}</p>
-          {qrImages[payment._id] ? (
-            <img
-              src={qrImages[payment._id]}
-              alt={`QR Code for ${payment.appName}`}
-            />
-          ) : (
-            <p>Loading QR code...</p>
-          )}
-        </div>
+    <Carousel className="w-full max-w-xs">
+    <CarouselContent>
+      {paymentMethod.map((payment, index) => (
+        <CarouselItem key={payment._id}>
+          <div className="p-1">
+            <Card>
+              <CardContent className="flex flex-col items-center justify-center p-4">
+                <p className="text-xl font-semibold mb-2">{payment.appName}</p>
+                <p className="text-sm text-gray-500 mb-2">{payment.paymentId}</p>
+                <p className="text-sm text-gray-500 mb-2">{payment.type}</p>
+                {qrImages[payment._id] ? (
+                  <img
+                    src={qrImages[payment._id]}
+                    alt={`QR Code for ${payment.appName}`}
+                    className="w-32 h-32 object-contain"
+                  />
+                ) : (
+                  <p>Loading QR code...</p>
+                )}
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
       ))}
-    </div>
+    </CarouselContent>
+    <CarouselPrevious />
+    <CarouselNext />
+  </Carousel>
   );
 };
