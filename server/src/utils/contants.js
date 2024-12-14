1888e204b0cd1c56ee536920935eec06d52cb6e2
Aniket <aniketbaranwal8090@gmail.com>
2024-12-14 21:19:57 +0530

    minor changes

--- server/src/utils/contants.js
+++ server/src/utils/contants.js
@@ -1,25 +1,19 @@
 export const RoomEventEnum = Object.freeze({
-    JOIN_ROOM_EVENT: "joinRoom",
-    LEAVE_ROOM_EVENT: "leaveRoom",
-    JOIN_ROOM_REQUEST_EVENT: "joinRoomRequest",
-    REQUEST_ROOM_RESPONSE_EVENT: "adminResponse",
-    DELETE_ROOM_EVENT: "deleteRoom",
-    UPDATE_ROOM_EVENT: "updateRoom",
+  JOIN_ROOM_EVENT: "joinRoom",
+  LEAVE_ROOM_EVENT: "leaveRoom",
+  JOIN_ROOM_REQUEST_EVENT: "joinRoomRequest",
+  REQUEST_ROOM_RESPONSE_EVENT: "adminResponse",
+  DELETE_ROOM_EVENT: "deleteRoom",
+  UPDATE_ROOM_EVENT: "updateRoom",
 });
 
-
 export const ChatEventEnum = Object.freeze({
-  
   JOIN_CHAT_EVENT: "joinChat",
   // ? when participant gets removed from group, chat gets deleted or leaves a group
   LEAVE_CHAT_EVENT: "leaveChat",
-  // ? when admin updates a group name
-  UPDATE_GROUP_NAME_EVENT: "updateGroupName",
   // ? when new message is received
   MESSAGE_RECEIVED_EVENT: "messageReceived",
   // ? when there is new one on one chat, new group chat or user gets added in the group
-  NEW_CHAT_EVENT: "newChat",
-  // ? when there is an error in socket
   SOCKET_ERROR_EVENT: "socketError",
   // ? when participant stops typing
   STOP_TYPING_EVENT: "stopTyping",
@@ -30,11 +24,11 @@ export const ChatEventEnum = Object.freeze({
 });
 
 export const TaskEventEnum = Object.freeze({
-  TASK_CREATE_EVENT: "connected",
-  TASK_DELETE_EVENT: "connected",
-  TASK_UPDATED_EVENT: "connected",
-  TASK_SWITCH_REQUEST_EVENT: "connected",
-  TASK_SWITCH_RESPONSE_EVENT: "connected",
+  TASK_CREATE_EVENT: "createdTask",
+  TASK_DELETE_EVENT: "deletedTask",
+  TASK_UPDATED_EVENT: "updatedTask",
+  TASK_SWITCH_REQUEST_EVENT: "requestSwitchTask",
+  TASK_SWITCH_RESPONSE_EVENT: "responseSwitchTask",
 });
 
 export const MaintenanceEventEnum = Object.freeze({
