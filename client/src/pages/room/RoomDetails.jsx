f5029a75a72b81c1847e24c730b14f50527b9c12
Aniket <aniketbaranwal8090@gmail.com>
2024-12-26 18:56:38 +0530

    Socket connectio persists

--- client/src/pages/room/RoomDetails.jsx
+++ client/src/pages/room/RoomDetails.jsx
@@ -4,10 +4,8 @@ import { useParams } from "react-router-dom";
 import Maintenance from "./Maintenance";
 import RoomExpense from "./RoomExpense";
 import RoomEvents from "./RoomEvents";
-import { useEffect } from "react";
 import { PollForm } from "@/components/form/PollForm";
 import PollVote from "@/components/PollVote";
-import { getSocket } from "@/socket";
 
 const RoomDetails = () => {
   const { roomId } = useParams();
@@ -15,21 +13,6 @@ const RoomDetails = () => {
   const { roomQuery } = useRoom(roomId);
   const { data, isLoading, isError } = roomQuery;
   // console.log(data);
-  const socket = getSocket();
-
-  useEffect(() => {
-    // Join room only if it's a new room
-
-    socket.emit("joinRoom", roomId);
-    console.log(`Joined room: ${roomId}`);
-
-    return () => {
-      // Leave room when the user navigates away from the room entirely
-
-      socket.emit("leaveRoom", roomId);
-      console.log(`Left room: ${roomId}`);
-    };
-  }, [roomId, socket]);
 
   if (isLoading) {
     return <Spinner />;
