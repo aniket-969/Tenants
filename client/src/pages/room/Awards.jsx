d44c00738e11e716064cce764bdca8b43de75e8c
Aniket <aniketbaranwal8090@gmail.com>
2024-12-28 19:13:27 +0530

    Added awards creation socket response

--- client/src/pages/room/Awards.jsx
+++ client/src/pages/room/Awards.jsx
@@ -3,19 +3,42 @@ import AwardCard from "../../components/awardCard";
 import { useAward } from "@/hooks/useAwards";
 import { Spinner } from "@/components/ui/spinner";
 import { AwardsForm } from "@/components/form/AwardsForm";
+import { useEffect, useState } from "react";
+import { getSocket } from "@/socket";
 
 const Awards = () => {
   const { roomId } = useParams();
   const { awardsQuery } = useAward();
-
+  const [awards, setAwards] = useState([]);
   const { isLoading, data, isError } = awardsQuery(roomId);
+
+  useEffect(() => {
+    if (data) {
+      setAwards(data);
+    }
+  }, [data]);
+
+
+  const socket = getSocket();
+  
+    useEffect(() => {
+      const handleCreateAward = (newAward) => {
+        console.log("create it");
+        setAwards((prevAward) => [...prevAward, newAward]);
+      };
+      socket.on("createAward", handleCreateAward);
+  
+      return () => {
+        socket.off("createAward", handleCreateAward);
+      };
+    }, [socket]);
+
   if (isLoading) {
     return <Spinner />;
   }
   if (isError) {
     return <div> Something went wrong. Please refresh or login again</div>;
   }
-  console.log(data);
 
   return (
     <div className=" p-6 ">
@@ -25,9 +48,9 @@ const Awards = () => {
       <div className="flex justify-center">
         <AwardsForm />
       </div>
-      
+
       <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 lg:gap-10 content-center ">
-        {data.map((award) => (
+        {awards.map((award) => (
           <AwardCard
             key={award._id}
             title={award.title}
