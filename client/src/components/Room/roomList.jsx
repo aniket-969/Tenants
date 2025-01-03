33fe5073358a512a46ada60dd356fa9dba925fad
Aniket <aniketbaranwal8090@gmail.com>
2025-01-04 00:15:04 +0530

    Adding scrollBar shadcn component

--- client/src/components/Room/roomList.jsx
+++ client/src/components/Room/roomList.jsx
@@ -3,6 +3,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { Spinner } from "../ui/spinner";
 import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 const RoomList = () => {
   const { sessionQuery } = useAuth();
@@ -15,18 +16,49 @@ const RoomList = () => {
   if (isError) {
     return <>Something went wrong . Please refresh</>;
   }
+  const rooms = [
+    {
+      roomId: "6756e343b2fdac1824b18cc1",
+      name: "rolling stones",
+      _id: "6756e343b2fdac1824b18cc8",
+    },
+    {
+      roomId: "6756e3eab2fdac1824b18cea",
+      name: "U2",
+      _id: "6756e3ebb2fdac1824b18cf2",
+    },
+    {
+      roomId: "6756e421b2fdac1824b18d03",
+      name: "redhotchillipeppers",
+      _id: "6756e421b2fdac1824b18d0c",
+    },
+    {
+      roomId: "6756e90420e3d2b8bb7f3b47",
+      name: "Ameename",
+      _id: "6756e90420e3d2b8bb7f3b51",
+    },
+    {
+      roomId: "6756e90420e3d2b8bb7f3b47",
+      name: "Hmmm",
+      _id: "6756e90420e3d2b8bb7f3b52",
+    },
+  ];
   return (
-    <div className="flex flex-col gap-5">
-      {data.rooms.map((room) => (
-        <Link key={room._id} to={`/room/${room.roomId}`}>
-          <Button
-            className="text-white text-lg w-full rounded-none "
-            variant="outline"
-          >
-            {room.name}
-          </Button>
-        </Link>
-      ))}
+    <div className="flex flex-col gap-5 items-center">
+      <h1 className="text-xl font-semibold">Rooms</h1>
+<ScrollArea>
+    <div className="flex flex-col gap-5 h-[15rem] pt-2">
+        {rooms.map((room) => (
+          <Link key={room._id} to={`/room/${room.roomId}`}>
+            <Button className="text-white text-lg w-full rounded-none "
+              variant="outline">
+              {room.name}
+            </Button>
+          </Link>
+        ))}
+      </div>
+</ScrollArea>
+    
     </div>
   );
 };
