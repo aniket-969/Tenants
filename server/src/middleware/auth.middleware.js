9a58a34308f931cf0d7043980c97ad843d0a1680
Aniket <aniketbaranwal8090@gmail.com>
2025-01-30 22:11:29 +0530

    Updated jwt expiry error from breaking server

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -2,31 +2,42 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
-
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-  const token =
-    req.cookies?.accessToken ||
-    req.header("Authorization")?.replace("Bearer", "");
-  // console.log("cookies", req.cookies);
-  if (!token) {
-    throw new ApiError(401, "Unauthorized request");
-  }
-  
-  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  // console.log("decodedToken", decodedToken);
-  if (decodedToken.exp * 1000 < Date.now()) {
-    throw new ApiError(401, "Token has expired");
-  }
-  const user = await User.findById(decodedToken?._id).select(
-    "-password -refreshToken -rooms"
-  );
-  // console.log("jwt verification", user);
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
 
-  if (!user) {
-    throw new ApiError(401, "Invalid access token");
-  }
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request. Token is missing.");
+    }
 
-  req.user = user;
-  next();
+    let decodedToken;
+    try {
+      decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        throw new ApiError(401, "Unauthorized request. Token has expired.");
+      } else {
+        throw new ApiError(401, "Unauthorized request. Invalid token.");
+      }
+    }
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken -rooms"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Unauthorized request. User not found.");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("JWT verification error:", error.message);
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
 });
- 
