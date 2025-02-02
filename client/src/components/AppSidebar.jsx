b5759a191094176c030ba712e42703b7c0378b98
Aniket <aniketbaranwal8090@gmail.com>
2025-02-02 18:05:26 +0530

    Adding dynamic url for navbar

--- client/src/components/AppSidebar.jsx
+++ client/src/components/AppSidebar.jsx
@@ -1,4 +1,4 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Award, Calendar, Home, Inbox, Search, Settings } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
@@ -8,48 +8,45 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
-
-// Menu items
-const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
-  },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
-  },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
-  },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
-  },
-]
+} from "@/components/ui/sidebar";
+import { useEffect, useState } from "react";
 
 export function AppSidebar() {
+  const [roomId, setRoomId] = useState(null);
+  useEffect(() => {
+    const storedRoomId = localStorage.getItem("currentRoomId");
+    setRoomId(storedRoomId);
+  }, []);
+  // Menu items with dynamic roomId
+  const items = [
+    { title: "Home", url: "/room", icon: Home },
+    { title: "Room", url: roomId ? `/room/${roomId}` : "/room", icon: Inbox },
+    {
+      title: "Awards",
+      url: roomId ? `/room/${roomId}/awards` : "/room/awards",
+      icon: Award,
+    },
+    {
+      title: "Events",
+      url: roomId ? `/room/${roomId}/events` : "/room/events",
+      icon: Settings,
+    },
+    {
+      title: "Expense",
+      url: roomId ? `/room/${roomId}/expense` : "/room/expense",
+      icon: Settings,
+    },
+  ];
+
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
-         
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton 
+                  <SidebarMenuButton
                     asChild
                     tooltip={item.title} // This adds tooltips when sidebar is collapsed
                   >
@@ -65,5 +62,5 @@ export function AppSidebar() {
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
-  )
+  );
 }
