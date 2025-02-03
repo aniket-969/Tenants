25d64b23980de87998bf050371d8e28a915819f8
Aniket <aniketbaranwal8090@gmail.com>
2025-02-03 21:05:40 +0530

    Added getCurrentAssignee function for calendar tasks

--- client/src/pages/room/RoomCalendar.jsx
+++ client/src/pages/room/RoomCalendar.jsx
@@ -1,14 +1,21 @@
-import { Calendar } from "@/components/ui/calendar"
-import { useState } from "react"
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
 
-const RoomCalendar = ({tasks}) => {
-    console.log(tasks)
-    const [date,setDate] = useState(new Date())
+const RoomCalendar = ({ tasks }) => {
+  console.log(tasks);
+  const [date, setDate] = useState(new Date());
+  console.log(date)
   return (
-    <div>RoomCalendar
-       <Calendar mode="single" selected={date} onSelect={setDate}/>
+    <div>
+      RoomCalendar
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border shadow bmain"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default RoomCalendar
+export default RoomCalendar;
--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -2,16 +2,33 @@ import jsQR from "jsqr";
 import QRCode from "qrcode";
 
 export const generateAvatarUrls = (count) => {
-    return Array.from({ length: count }, (_, index) => 
-      `https://avatar.iran.liara.run/public/${index + 1}`
-    );
-  };
-  
-  export const generateQRCode = async (text) => {
-    try {
-      const qrCodeUrl = await QRCode.toDataURL(text);
-      return qrCodeUrl;
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  return Array.from(
+    { length: count },
+    (_, index) => `https://avatar.iran.liara.run/public/${index + 1}`
+  );
+};
+
+export const generateQRCode = async (text) => {
+  try {
+    const qrCodeUrl = await QRCode.toDataURL(text);
+    return qrCodeUrl;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const getCurrentAssignee = (task, currentDate) => {
+  const { rotationOrder, dueDate, currentAssignee } = task;
+
+  const dateDiff = Math.floor(
+    (currentDate - new Date(dueDate)) / (1000 * 60 * 60 * 24)
+  );
+
+  const currentIndex = rotationOrder.findIndex(
+    (user) => user.toString() === currentAssignee.toString()
+  );
+
+  const assigneeIndex = (currentIndex + dateDiff) % rotationOrder.length;
+
+  return rotationOrder[assigneeIndex];
+};
