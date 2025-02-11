c2bce72a6d1ce066c8c11431ba1ae546386ffa2b
Aniket <aniketbaranwal8090@gmail.com>
2025-02-12 00:10:09 +0530

    Modified calendar shadcn classnames to change UI

--- client/src/pages/room/Maintenance/Maintenance.jsx
+++ client/src/pages/room/Maintenance/Maintenance.jsx
@@ -29,18 +29,8 @@ const Maintenance = ({ maintenance }) => {
   }, [socket]);
 
   return (
-    <div className="flex flex-col items-center justify-center m-5">
-      {/* <div className="flex flex-col gap-3">
-        {maintenances.map((data) => (
-          <div key={data._id}>
-            <h2>{data.title}</h2>
-            <p>{data.description}</p>
-            <p>{data.maintenanceProvider}</p>
-            <p>{data.status}</p>
-            <p>{data.costEstimate}</p>
-          </div>
-        ))}
-      </div> */}
+    <div className="flex flex-col gap-6 w-full items-center ">
+      <h2 className="font-bold text-xl">Create Maintenance Request</h2>
       <FormWrapper>
         <MaintenanceForm />
       </FormWrapper>
