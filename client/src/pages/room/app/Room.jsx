bce3e378fc669483c092ed10da1d29e79f66a7a5
Aniket <aniketbaranwal8090@gmail.com>
2025-02-17 23:21:45 +0530

    Added Room Loader combining other skeletons for room

--- client/src/pages/room/app/Room.jsx
+++ client/src/pages/room/app/Room.jsx
@@ -1,9 +1,8 @@
+import RoomLoader from "@/components/Loader/Room";
 import ProfileCard from "@/components/ProfileCard";
 import QRCode from "@/components/QRCode";
 import { RoomHeader } from "@/components/Room/roomHeader";
 import RoomList from "@/components/Room/roomList";
-import QrCodeSkeleton from "@/components/skeleton/qrCode";
-import RoomListSkeleton from "@/components/skeleton/roomList";
 import { Navigate, Outlet } from "react-router-dom";
 
 const Room = () => {
@@ -12,16 +11,17 @@ const Room = () => {
   return session ? (
     <div className=" max-h-screen ">
       <RoomHeader />
-      <div className="flex flex-col gap-10 items-center pt-7 sm:pt-12">
+      {/* <div className="flex flex-col gap-10 items-center pt-7 sm:pt-12">
         <ProfileCard />
         <div className="flex flex-col-reverse justify-around items-center w-full gap-20 sm:gap-10 sm:flex-row sm:mt-7 ">
           <QRCode />
-          {/* <QrCodeSkeleton/> */}
+       
           <RoomList />
          
           
         </div>
-      </div>
+      </div> */}
+      <RoomLoader/>
     </div>
   ) : (
     <Navigate to="/login" />
