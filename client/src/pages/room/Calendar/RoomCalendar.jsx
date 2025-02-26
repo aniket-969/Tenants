b27732e8991327074ba7df1b874002429d43d456
Aniket <aniketbaranwal8090@gmail.com>
2025-02-26 16:49:07 +0530

    Integrating functions for task in roomCalendar

--- client/src/pages/room/Calendar/RoomCalendar.jsx
+++ client/src/pages/room/Calendar/RoomCalendar.jsx
@@ -1,9 +1,22 @@
 import { Calendar } from "@/components/ui/calendar";
+import { getTasksForDate } from "@/utils/helper";
 import { useEffect, useState } from "react";
 
 const RoomCalendar = ({ tasks }) => {
   const [date, setDate] = useState(new Date());
-// console.log(tasks)
+  // console.log(tasks)
+  const [scheduledTasks, setScheduledTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks && tasks.length > 0) {
+      const tasksForDate = getTasksForDate(tasks, date);
+      setScheduledTasks(tasksForDate);
+    }
+  }, [date, tasks]);
+  const hasTasksOnDate = (day) => {
+    return getTasksForDate(tasks, day).length > 0;
+  };
+  console.log(scheduledTasks)
   return (
     <div>
       RoomCalendar
