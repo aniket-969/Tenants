deade7d6815190998913ba2f3228129f549feb35
Aniket <aniketbaranwal8090@gmail.com>
2025-03-31 17:42:53 +0530

    Modifying formWrapper for a popup on click

--- client/src/components/ui/formWrapper.jsx
+++ client/src/components/ui/formWrapper.jsx
@@ -1,14 +1,24 @@
+import { useRef } from "react";
 import { cn } from "@/lib/utils";
 
-const FormWrapper = ({ children, className }) => {
+const FormWrapper = ({ children, className, onClose }) => {
+  const wrapperRef = useRef(null);
+
   return (
     <div
-      className={cn(
-        "bmain w-full max-w-[30rem] p-10 rounded-[2.5rem] ",
-        className
-      )}
+      className="fixed inset-0 bg-[#121212]/60 flex justify-center items-center"
+      onClick={onClose} // Clicking on overlay will close the modal
     >
-      {children}
+      <div
+        ref={wrapperRef}
+        className={cn(
+          "bmain w-full max-w-[30rem] p-10 rounded-[2.5rem] bg-black",
+          className
+        )}
+        onClick={(e) => e.stopPropagation()} 
+      >
+        {children}
+      </div>
     </div>
   );
 };
--- client/src/pages/room/Expense/RoomExpense.jsx
+++ client/src/pages/room/Expense/RoomExpense.jsx
@@ -1,38 +1,46 @@
+import { useState } from "react";
 import { ExpenseForm } from "@/components/form/ExpenseForm";
 import FormWrapper from "@/components/ui/formWrapper";
 import { Spinner } from "@/components/ui/spinner";
 import { useExpense } from "@/hooks/useExpense";
 import { useRoom } from "@/hooks/useRoom";
-import { getSocket } from "@/socket";
-import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 
 const RoomExpense = () => {
-  
   const { roomId } = useParams();
-
   const { createExpenseMutation } = useExpense(roomId);
   const { roomQuery } = useRoom(roomId);
   const { data, isLoading, isError } = roomQuery;
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
-  if (isLoading) {
-    return <Spinner />;
-  }
-  if (isError) {
-    return <>Something went wrong . Please refresh</>;
-  }
+  if (isLoading) return <Spinner />;
+  if (isError) return <>Something went wrong. Please refresh</>;
 
   const participants = [
     ...(data.tenants || []),
     ...(data.landlord ? [data.landlord] : []),
   ];
+
   return (
-    <div className="flex flex-col gap-6 w-full items-center ">
+    <div className="flex flex-col gap-6 w-full items-center">
       <h2 className="font-bold text-xl">Split Expense</h2>
 
-      <FormWrapper>
-        <ExpenseForm participants={participants} />
-      </FormWrapper>
+      <Button
+        onClick={() => setIsFormOpen(true)}
+       
+      >
+        Create Expense
+      </Button>
+
+      {isFormOpen && (
+        <FormWrapper onClose={() => setIsFormOpen(false)}>
+          <ExpenseForm
+            participants={participants}
+            onSubmit={() => setIsFormOpen(false)} // Close on submit
+          />
+        </FormWrapper>
+      )}
     </div>
   );
 };
