d9b4612334e194f856046577ae3f9b9e0fd27f69
Aniket <aniketbaranwal8090@gmail.com>
2025-04-03 18:48:02 +0530

    Updating chatInput textArea and send icon

--- client/src/components/Chat/ChatInput.jsx
+++ client/src/components/Chat/ChatInput.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Send } from "lucide-react"; // Importing Send icon
+import { SendHorizontal } from "lucide-react"; // Importing SendHorizontal  icon
 import { Textarea } from "@/components/ui/textarea"; // ShadCN Textarea
 import { Button } from "@/components/ui/button";
 import { useChat } from "@/hooks/useChat";
@@ -11,7 +11,7 @@ const ChatInput = ({ onSendMessage }) => {
   const { roomId } = useParams();
 
   const handleSendMessage = async () => {
-    if (!content.trim()) return; // Prevent empty messages
+    if (!content.trim()) return;
     onSendMessage(content);
     await sendMessageMutation.mutateAsync({ data: { content }, roomId });
     setContent("");
@@ -19,27 +19,24 @@ const ChatInput = ({ onSendMessage }) => {
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault(); // Prevent new line
+      e.preventDefault();
       handleSendMessage();
     }
   };
 
   return (
-    <div className="flex items-center p-2 border-t">
+    <div className="flex items-center p-2 border-t gap-2">
       <Textarea
         value={content}
         onChange={(e) => setContent(e.target.value)}
         onKeyDown={handleKeyDown}
         placeholder="Type a message..."
-        className="flex-1 resize-none overflow-hidden rounded-md px-4 py-2 border focus:outline-none focus:ring-2 "
-        rows={1} // Keeps it single-line initially
+        className=" overflow-hidden rounded-md px-4 py-2 border focus:outline-none focus:ring-2 "
+        rows={1}
       />
       {content.trim() && (
-        <Button
-          onClick={handleSendMessage}
-          className="ml-2 p-2 rounded-full bg-primary text-white hover:bg-primary-dark"
-        >
-          <Send className="w-5 h-5" />
+        <Button onClick={handleSendMessage} className="  rounded-full">
+          <SendHorizontal />
         </Button>
       )}
     </div>
