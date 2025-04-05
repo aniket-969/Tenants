0eabc2777c7c4ed16be392befbab1cb06b2b6ffc
Aniket <aniketbaranwal8090@gmail.com>
2025-04-05 21:28:52 +0530

    Added RoomMembers component

--- client/src/components/AppSidebar.jsx
+++ client/src/components/AppSidebar.jsx
@@ -26,6 +26,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { useParams } from "react-router-dom";
 import { useRoom } from "@/hooks/useRoom";
 import { useState } from "react";
+import RoomMembers from "./Sidebar/RoomMembers";
 
 export function AppSidebar() {
   const { roomId } = useParams();
@@ -64,27 +65,7 @@ export function AppSidebar() {
         </div>
 
         {/* Room Members */}
-        <div className="p-4 border-b">
-          <div className="flex items-center text-sm cursor-pointer space-x-2" onClick={toggleMembers}>
-            <Users className="w-5 h-5" />
-            <span className="flex-1 font-medium ">Members</span>
-            <span className="text-xs bg-secondary px-2 text-black font-semibold py-1 rounded-full">{roomData?.tenants?.length || 0}</span>
-          </div>
-          {showMembers && (
-            <div className="mt-3">
-              <ScrollArea className="h-[104px]">
-                <div className="space-y-2 pr-3">
-                  {roomData?.tenants?.map((member) => (
-                    <div key={member._id} className="flex items-center p-2 bg-secondary/20 rounded-lg">
-                      <img src={member.avatar} alt={member.fullName} className="w-8 h-8 rounded-full mr-3 border" />
-                      <span className="text-sm font-medium ">{member.fullName}</span>
-                    </div>
-                  ))}
-                </div>
-              </ScrollArea>
-            </div>
-          )}
-        </div>
+      <RoomMembers toggleMembers={toggleMembers} showMembers={showMembers} tenants={roomData?.tenants}/>
 
         {/* Pending Requests */}
        {roomData?.pendingRequests && <div className="p-4 border-b">
