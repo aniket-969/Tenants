ce41a2f21a7c91674ca2ed283205f36d6e193a32
Aniket <aniketbaranwal8090@gmail.com>
2025-04-15 18:30:59 +0530

    Resolved shadcn calendar resizing problem

--- client/src/pages/room/Calendar/RoomCalendar.jsx
+++ client/src/pages/room/Calendar/RoomCalendar.jsx
@@ -21,10 +21,18 @@ const RoomCalendar = ({ tasks }) => {
 
       {/* Calendar Component */}
       <Calendar
+        classNames={{
+          months:
+            "flex w-full flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0 flex-1",
+          month: "space-y-4 w-full flex flex-col",
+          table: "w-full h-full border-collapse space-y-1",
+          head_row: "",
+          row: "w-full mt-2",
+        }}
         mode="single"
         selected={date}
         onSelect={setDate}
-        className="w-full rounded-xl border shadow bmain"
+        className=" rounded-xl bmain h-[300px]"
       />
 
       {/* Scheduled Tasks */}
